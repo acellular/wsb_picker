a66dab9399a544909b1910f7f42817a0bd0bdfca
ACELLULAR <mchltuck@gmail.com>
2022-08-21 16:57:09 -0400

    added easy hidden layer sizing

--- PrintToCSV.py
+++ PrintToCSV.py
@@ -10,7 +10,6 @@ import StockChecker
 def counts_organized(csv_file, stocks_data, words, indicators):
     #stocks now instead stocks_data-->AND PULL DATE FROM STOCKS_DATA!
 
-
     with open(csv_file, "w", encoding="utf-8") as text_file: # "a" for appending
         text_file.write('Date,Stock,Percent Change (i.e. y),Count,S&P500Change,SENTIMENT-polarity,SENTIMENT-subjectivity')
         for indicator in indicators:
@@ -20,11 +19,11 @@ def counts_organized(csv_file, stocks_data, words, indicators):
         
 
         # pull stock data and add to csv
-        #TODO--currently combinging stocks and date so don't have to change log regress
+        # TODO--currently combinging stocks and date so don't have to change log regress
         for sD in stocks_data:
-            #day, stock, count, change, SPchange
+            #  day, stock, count, change, SPchange
             text_file.write(f"\n{sD[0]},{sD[1]},{sD[2]},{sD[5]['Count']},{sD[3]}")
-            #sentiment
+            # sentiment
             text_file.write(f",{sD[5]['SENTIMENT-polarity']},{sD[5]['SENTIMENT-subjectivity']}")  
             #print(sD[5])
             for sDindict in sD[4]: #indicators
@@ -53,3 +52,6 @@ def word_counts_stocks(csv_file, stocks_data, words):
             #print(sD[5])
             for word in words: #word counts
                 text_file.write(',' + str(sD[5][word]))
+
+#def reddit_must_haves(csv_file, stocks_data, words):
+#    with open (csv_file, 'w')
--- torchMLP.py
+++ torchMLP.py
@@ -3,13 +3,28 @@ from collections import OrderedDict
 import torch
 from torch.optim import SGD #stochastic gradient descent
 
-#basic neural net
-def simple_mlp_nn(features, hidden_dim, classes):
+# basic neural net
+def simple_mlp_nn(features, nodes, classes):
     mlpModel = torch.nn.Sequential(OrderedDict([
-        ("hidden_layer", torch.nn.Linear(features, hidden_dim)),
+        ("input_layer", torch.nn.Linear(features, nodes)),
         ("activation", torch.nn.ReLU()),
-        ("output", torch.nn.Linear(hidden_dim, classes))]))
+        ("output", torch.nn.Linear(nodes, classes))]))
     return mlpModel
+
+# mlp with easily definable layer sizes (like sklearn's MLP)
+def mlp_nn(features, classes, hidden_layer_sizes=(200,100)):
+    layers = OrderedDict()
+    layers['input_layer'] = torch.nn.Linear(features, hidden_layer_sizes[0])
+    layers['activation_input'] = torch.nn.ReLU()
+    for i in range(1,len(hidden_layer_sizes)):
+        layers[f'hidden_layer_{i}'] = torch.nn.Linear(hidden_layer_sizes[i-1], hidden_layer_sizes[i])
+        layers[f'activation_{i}'] = torch.nn.ReLU()
+
+    layers['output'] = torch.nn.Linear(hidden_layer_sizes[len(hidden_layer_sizes)-1], classes)
+    mlpModel = torch.nn.Sequential(layers)
+    return mlpModel
+
+# TODO--a model for 2D+ data that starts with Flatten()?
 
 def next_batch(X, y, batch_size): #inputs (X), targets (y)
     # loop over the dataset
@@ -22,14 +37,10 @@ def next_batch(X, y, batch_size): #inputs (X), targets (y)
 # (remember must also put mlp into training or evaluation modes first)
 def train_assess(X, y, mlp, dev="cpu", lr=1e-2, weight_decay=0):
     optimizer = SGD(mlp.parameters(), lr=lr, weight_decay=weight_decay)#WEIGHT DECAY IS REGULARIZATION
-    loss_func = torch.nn.CrossEntropyLoss() #TODO--look up loss functions
+    #loss_func = torch.nn.L1Loss() #TODO--look up more loss functions
+    loss_func = torch.nn.CrossEntropyLoss() 
 
-    loss_score = 0 #initialize loss
-    accuracy = 0 #initialize accuracy 
-    samples = 0
-
-    # flash data to the current dev, run it through our
-    # model, and calculate loss
+    # send data to device, run model, calculate loss
     X, y = (X.to(dev), y.to(dev))
     y_pred = mlp(X)#THE ACTUAL NEURAL NET!
     loss = loss_func(y_pred, y.long())
@@ -42,26 +53,25 @@ def train_assess(X, y, mlp, dev="cpu", lr=1e-2, weight_decay=0):
         optimizer.step() #update model parameters (i.e. weights)
     
     # update training loss, accuracy, and # samples visited
-    loss_score += loss.item() * y.size(0)
-    accuracy += (y_pred.max(1)[1] == y).sum().item()
-    samples += y.size(0)
+    loss_score = loss.item() * y.size(0)
+    accuracy = (y_pred.max(1)[1] == y).sum().item()
+    samples = y.size(0)
     return loss_score, accuracy, samples
 
 
 # for more optimized training using smaller samples for each model iteration
 def batch_train(X_train, X_test, y_train, y_test, batch_size=64, epochs=10,
-    lr=1e-2, hidden_dim=2, classes=None, verbose=False, weight_decay=0):
+    lr=1e-2, hidden_layer_sizes=[2], classes=None, verbose=False, weight_decay=0):
 
     # can use CUDA?
     #A THING FOR DEBUGGING CUDA ERRORS
     #import os
     #os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
     dev = "cuda" if torch.cuda.is_available() else "cpu"
-    print("[INFO] training using {}...".format(dev))
+    print(f"Device: {dev}")
 
     # data shape
     num_rows, num_cols = X_train.shape
-    print (num_cols)
     if classes==None: unique = numpy.unique(y_train)
 
     # convert to PyTorch tensors
@@ -71,31 +81,25 @@ def batch_train(X_train, X_test, y_train, y_test, batch_size=64, epochs=10,
     y_test = torch.from_numpy(y_test).float()
 
     # initialize model and print shape
-    mlp = simple_mlp_nn(num_cols, hidden_dim, len(unique)).to(dev)
+    mlp = mlp_nn(num_cols, len(unique), hidden_layer_sizes=hidden_layer_sizes).to(dev)
     print(mlp)
-
-    # create a template to summarize current training progress
-    training_printout = "epoch: {} test loss: {:.3f} test accuracy: {:.3f}"
 
     # loop epochs
     for epoch in range(0, epochs):
         
-        print(f"Epoch: {epoch + 1}...")
+        if epoch % 10 == 0: print(f"Epoch {epoch}...")
 
         # training mode
-        loss_train = 0 #initialize loss
-        accuracy_train = 0 #initialize accuracy 
+        loss_train = 0
+        accuracy_train = 0
         samples_train = 0
-        mlp.train() #put into training mode so model parameters to be updated during backpropagation.
+        mlp.train() #put into training mode so model parameters updated during backpropagation.
         # loop the current batch
         for (X_batch, y_batch) in next_batch(X_train, y_train, batch_size):
             loss, accuracy, samples = train_assess(X_batch, y_batch, mlp, dev=dev, lr=lr)
             loss_train += loss
             accuracy_train += accuracy
             samples_train += samples
-        # display model progress on the current training batch
-        if verbose: print('TRAIN: ', training_printout.format(epoch + 1, (loss_train / samples_train),
-            (accuracy_train / samples_train)))
         
         # evaluation mode
         loss_test = 0
@@ -108,13 +112,14 @@ def batch_train(X_train, X_test, y_train, y_test, batch_size=64, epochs=10,
                 loss_test += loss
                 accuracy_test += accuracy
                 samples_test += samples
-        # display model progress on the current test batch
-        if verbose: print('TEST: ',training_printout.format(epoch + 1, (loss_test / samples_test),
-            (accuracy_test / samples_test)))
-
+        # display model progress on current test batch
+        if verbose: print(f'Epoch {epoch}: Train loss: {loss_train / samples_train:.3f} accuracy: {accuracy_train / samples_train:.3f},',
+            f'Test loss: {loss_test / samples_test:.3f} accuracy: {accuracy_test / samples_test:.3f}')
     print(f'Training finished. Loss: {loss_test / samples_test} Accuracy: {accuracy_test / samples_test}')
     return mlp
 
+
+# testing
 if __name__ == "__main__":
     from sklearn.datasets import make_blobs
     from sklearn.model_selection import train_test_split 
@@ -122,7 +127,7 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)#, random_state=95)
 
     mlp = batch_train(X_train, X_test, y_train, y_test,
-        batch_size=64, epochs=10, lr=1e-2, hidden_dim=8, verbose=True)
+        batch_size=64, epochs=10, lr=1e-2, hidden_layer_sizes=(2000,3000,100), verbose=True)
             
 
 """
